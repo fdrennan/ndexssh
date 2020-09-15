bc620ce7dc1486ecf9f9c1d3bccee7e1cfbe8c31
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-15 14:59:42 -0600

    update

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ls -lah
-apt-get update -y
-git clone https://github.com/fdrennan/docker_pull_postgres.git || echo '\nDirectory already exists..\n'
-# ls docker_pull_postgres
-docker-compose -f docker_pull_postgres/docker-compose.yml pull
-docker-compose -f docker_pull_postgres/docker-compose.yml down
-docker-compose -f docker_pull_postgres/docker-compose.yml up -d
-docker container ls
